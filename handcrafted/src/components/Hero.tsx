67bd86e7314864c505f76abb47042bb232cd58d4
Emediong Henry Edet <ede21005@byui.edu>
2023-11-17 20:24:04 +0100

    Hero updated

--- handcrafted/src/components/Hero.tsx
+++ handcrafted/src/components/Hero.tsx
@@ -1,20 +1,20 @@
 export default function Hero () {
     return (
         <div className="bg-cover hg-100%" style={{ 
-            backgroundImage: `url("/images/landing.svg")`, 
+            backgroundImage: `url("/images/background.svg")`, 
         }}>
             <div className="grid grid-cols-4 h-full p-8 mb-2 bg-gray-600 mt-4 ml-20" >
-                <div className="border-solid border-4 border-text max-w-md mt-16 p-8 mb-64 rounded-2xl col-start-1 col-end-3 row-start-1 row-end-4">
-                    <h1 className="font-bold underline">You are Welcome to Handcrafted Haven</h1>
-                    <p className="text-text text-opacity-100">Handcrafted Haven is an innovative web application that aims to 
+                <div className="border-white bg-opacity-100 border-solid border-2 max-w-md mt-16 p-8 mb-64 rounded-2xl col-start-1 col-end-3 row-start-1 row-end-4">
+                    <h1 className="font-bold underline text-white">You are Welcome to Handcrafted Haven</h1>
+                    <p className="text-white text-opacity-100 mt-4">Handcrafted Haven is an innovative web application that aims to 
                         provide a platform for artisans and crafters to showcase and sell
                         their unique handcrafted items. It serves as a virtual marketplace, connecting 
                         talented creators with potential customers who appreciate the beauty 
                         and quality of handmade products. 
                     </p>
                 </div>
-                <div className="border-secondary bg-opacity-100 max-w-md border-solid border-4 rounded-2xl mt-64 mb-2 p-8 col-start-3 col-end-5 h-auto shadow-2xl opacity-100">
-                    <p>Handcrafted Haven aims to revolutionize the way handcrafted items are discovered, 
+                <div className="border-white bg-opacity-40 max-w-md border-solid border-2 rounded-2xl mt-64 mb-2 p-8 col-start-3 col-end-5 h-auto">
+                    <p className="text-white">Handcrafted Haven aims to revolutionize the way handcrafted items are discovered, 
                         appreciated, and acquired. By providing a digital platform for artisans to showcase 
                         their creativity and connect with a broader audience, the web application fosters a 
                         thriving community of passionate creators and conscious consumers. 
--- handcrafted/tailwind.config.js
+++ handcrafted/tailwind.config.js
@@ -24,10 +24,33 @@ module.exports = {
       muted: '#9E9E9E', // Medium 
       black: '#000000', // black 
       white: '#ffffff',
+      pink: ' #ed64a6',
+    },
+    opacity: {
+      '0': '0',
+      '20': '0.2',
+      '40': '0.4',
+      '60': '0.6',
+      '80': '0.8',
+      '100': '1',
     },
     fontFamily: {
       body: ['Roboto', 'Helvetica', 'Arial'],
       heading: ['Montserrat', 'sans-serif', 'serif'],
+    },
+    boxShadow: {
+      xs: '0 0 0 1px rgba(0, 0, 0, 0.05)',
+      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+      default: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+      md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+      '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+      '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+      outline: '0 0 0 3px rgba(66, 153, 225, 0.5)',
+      focus: '0 0 0 3px rgba(66, 153, 225, 0.5)',
+      none: 'none',
     },
     extend: {
       spacing: {
@@ -60,7 +83,7 @@ module.exports = {
         'full': '9999px',
         '4xl': '2rem',
 
-      }
+      },
     }
   },
 }
