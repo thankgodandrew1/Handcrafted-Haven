547c2969b49206c9ce8d3c9e3d07a0297b715786
Emediong Henry Edet <ede21005@byui.edu>
2023-11-17 20:30:00 +0100

    Merge changes from Footer.js

--- handcrafted/src/components/Layout.tsx
+++ handcrafted/src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "./Footer";
 
-export default function Layout({ children }) {
+export default function Layout({children}) {
     return (
         <div className="main-layout">
             <main>{children}</main>
