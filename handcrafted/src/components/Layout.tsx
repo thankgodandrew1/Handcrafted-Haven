0d4c6a76f63318f55dd144b89b5b9794fef34979
Omachona Richard <jineepinee@gmail.com>
2023-11-18 01:58:08 +0100

    Header

--- handcrafted/src/components/Layout.tsx
+++ handcrafted/src/components/Layout.tsx
@@ -1,9 +1,10 @@
-import React from "react";
 import Footer from "./Footer";
+import Header from "./Header";
 
 export default function Layout() {
     return (
         <div className="main-layout">
+            <Header />
             <Footer />
         </div>
     )
