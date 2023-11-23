63f2d2cc83577392e1981b4727c9c62c8651a898
Emediong Henry Edet <ede21005@byui.edu>
2023-11-23 14:37:42 +0100

    New files

---
+++ handcrafted/src/pages/ProductListing.tsx
@@ -0,0 +1,13 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import Layout from '@/components/Layout';
+import ProductListings from '@/components/Listings';
+
+
+export default function ProductListingPage() {
+    return (
+        <Layout>
+            <ProductListings />
+        </Layout>
+            
+    )
+}
