2d7f0042c917ce7ead6c3f6c33300e4e4dbbf3d8
thankgodandrew1 <and22046@byui.edu>
2023-12-02 14:13:14 -0700

    ..

--- handcrafted/next.config.js
+++ handcrafted/next.config.js
@@ -7,8 +7,8 @@ const nextConfig = {
   },
   publicRuntimeConfig: {
       apiUrl: process.env.NODE_ENV === 'development'
-          ? 'http://localhost:3001/api' // development api
-          : 'http://localhost:3001/api' // production api
+          ? 'http://localhost:3000/api' // development api
+          : 'http://localhost:3000/api' // production api
   }
 }
 
